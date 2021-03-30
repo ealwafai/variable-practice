d0a10e317f13f1d3a70700d0b1f86df1a9021a6c
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-03-29 23:26:07 -0600

    Add Hash variables

--- model-1.rb
+++ model-1.rb
@@ -12,3 +12,10 @@ is_landlocked = false
 languages_spoken = ["Arabic", "French", "English"]
 local_currencies = ["Lira", "Dollar"]
 touristic_places = ["Downtown Beirut", "beaches", "mountains"]
+
+heighest_peaks_in_ft = {"Mount Sannine" => 8842, "Mount Hermon" => 9232
+                        "Qurnat Sawda" => 10131}
+
+largest_cities_by_population = {"Beirut" => 1916100, "Tripoli" => 730000
+                                "Mount Lebanon" => 450000}
+                                
